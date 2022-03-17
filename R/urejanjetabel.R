7c30d3906c8968ea3421109b2fd050dd9fc58e42
Bor <bor.bregant@gmail.com>
2022-03-17 18:04:30 +0100

    roxygen goes brrr s kodo ki je malo boljsa

--- R/urejanjetabel.R
+++ R/urejanjetabel.R
@@ -10,11 +10,6 @@
 #' @examples
 #' urejanjetabel()
 
-
-library(plyr)
-library(purrr)
-library(dplyr)
-
 urejanjetabel <- function(kolokvij1# brez dodanih praznih "ucencev" ne deluje pravilno
                           ,kolokvij2=data.frame(IME=c(0),PRVA=c(0),DRUGA=c(0),TRETJA=c(0))
                           ,kolokvij3=data.frame(IME=c(0),PRVA=c(0),DRUGA=c(0),TRETJA=c(0))
@@ -26,30 +21,33 @@ urejanjetabel <- function(kolokvij1# brez dodanih praznih "ucencev" ne deluje pr
                           ,skupnotock
                           ,zapisi=FALSE
 ){
+  library(plyr)
+  library(purrr)
+  library(dplyr)
   kolokvij1$VSOTA <- rowSums(kolokvij1[2:ncol(kolokvij1)])
   kolokvij1 <- kolokvij1[,as.vector(c("IME","VSOTA"))]
 
   kolokvij2$VSOTA <- rowSums(kolokvij2[2:ncol(kolokvij2)])
   kolokvij2 <- kolokvij2[,as.vector(c("IME","VSOTA"))]
-  
+
   kolokvij3$VSOTA <- rowSums(kolokvij3[2:ncol(kolokvij3)])
   kolokvij3 <- kolokvij3[,as.vector(c("IME","VSOTA"))]
-  
+
   kolokvij4$VSOTA <- rowSums(kolokvij4[2:ncol(kolokvij4)])
   kolokvij4 <- kolokvij4[,as.vector(c("IME","VSOTA"))]
-  
+
   kolokvij5$VSOTA <- rowSums(kolokvij5[2:ncol(kolokvij5)])
   kolokvij5 <- kolokvij5[,as.vector(c("IME","VSOTA"))]
-  
+
   kolokvij6$VSOTA <- rowSums(kolokvij6[2:ncol(kolokvij6)])
   kolokvij6 <- kolokvij6[,as.vector(c("IME","VSOTA"))]
-  
+
   kolokvij7$VSOTA <- rowSums(kolokvij7[2:ncol(kolokvij7)])
   kolokvij7 <- kolokvij7[,as.vector(c("IME","VSOTA"))]
-  
+
   kolokvij8$VSOTA <- rowSums(kolokvij8[2:ncol(kolokvij8)])
   kolokvij8 <- kolokvij8[,as.vector(c("IME","VSOTA"))]
-  
+
   SKUPNATABELA <- purrr::reduce(list(kolokvij1,kolokvij2,kolokvij3,kolokvij4,kolokvij5,kolokvij6,kolokvij7,kolokvij8), dplyr::full_join, by = 'IME')
   SKUPNATABELA[is.na(SKUPNATABELA)] <- 0
   SKUPNATABELA$VSOTA <- rowSums(SKUPNATABELA[2:ncol(SKUPNATABELA)])
@@ -59,7 +57,7 @@ urejanjetabel <- function(kolokvij1# brez dodanih praznih "ucencev" ne deluje pr
   else{#                                          ce podamo max tocke vrne ime in procent
     SKUPNATABELA$VSOTA <- SKUPNATABELA$VSOTA / skupnotock
     names(SKUPNATABELA)[names(SKUPNATABELA) == 'VSOTA'] <- 'PROCENT'
-    
+
     ocena = rep(0, nrow(SKUPNATABELA))#vektor ocen (R deluje hitreje ce ne rabi sproti generirati)
     for(i in 1:nrow(SKUPNATABELA)){
       r = if (SKUPNATABELA$PROCENT[i] > 0.9) {
@@ -77,8 +75,8 @@ urejanjetabel <- function(kolokvij1# brez dodanih praznih "ucencev" ne deluje pr
     }
     SKUPNATABELA$OCENA <- ocena
     SKUPNATABELA <- SKUPNATABELA[,as.vector(c("IME","OCENA"))]
-    
+
     if (zapisi==TRUE) {write.table(SKUPNATABELA,"C:\\Users\\bor\\SKUPNATABELA.csv", row.names = FALSE)}
-    
+
     return(SKUPNATABELA)}
 }
